3c6f7e54b3de22fcc3150da42d29bb66b5a48644
pxalcantara <pxalcantara@gmail.com>
2020-08-08 18:34:26 -0300

    Add NVIDIA do the image and container.
    Signed-off-by: pxalcantara <pxalcantara@gmail.com>

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,19 +2,35 @@
 
 #creat image from Dockerfile
 # noetic_env is the image name
-docker build . -t "noetic_env"
+docker build -t noetic_env .
 
 # enable access from root container to X11 of the host
 xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
 
 #creat the container from the image created before
 #noetic_env is the container name
 docker run -it \
     --name="noetic_env" \
-    -e DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -e DISPLAY=${DISPLAY} \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
     -v ~/docker_noetic_env/docker/home-host:/root/home \
-    noetic_env
-    /bin/bash
+    noetic_env \
+    bash
+
